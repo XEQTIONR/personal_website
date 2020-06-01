ca8b8b08f4aff39d518e4bc3a8ee09181da0972e
Ishtehar Hussain <x.e.q.tionrz@gmail.com>
2020-06-01 15:02:01 -0400

    Making logout form work

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -193,8 +193,17 @@
             </div>
             <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
             <div class="modal-footer">
+
                 <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                <a class="btn btn-primary" href="login.html">Logout</a>
+                <button type="submit" class="btn btn-primary"
+                        onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+
+                Logout
+                </button>
+                <form id="logout-form d-none" method="POST" action="{{route('logout')}}">
+                    @csrf
+                </form>
             </div>
         </div>
     </div>
