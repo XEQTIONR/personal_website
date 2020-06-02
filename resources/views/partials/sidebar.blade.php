0708bcadd0d20e29da7684f3da6c0b932a944352
Ishtehar Hussain <x.e.q.tionrz@gmail.com>
2020-06-01 22:34:29 -0400

    Navbar items update

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -12,51 +12,51 @@
     <hr class="sidebar-divider my-0">
 
     <!-- Nav Item - Dashboard -->
-    <li class="nav-item active">
-        <a class="nav-link" href="index.html">
+    <li class="nav-item @if(Route::currentRouteName()=='home') active @endif">
+        <a class="nav-link" href="{{route('home')}}">
             <i class="fas fa-fw fa-tachometer-alt"></i>
             <span>Dashboard</span></a>
     </li>
 
-    <!-- Divider -->
-    <hr class="sidebar-divider">
+{{--    <!-- Divider -->--}}
+{{--    <hr class="sidebar-divider">--}}
 
-    <!-- Heading -->
-    <div class="sidebar-heading">
-        Static Content
-    </div>
+{{--    <!-- Heading -->--}}
+{{--    <div class="sidebar-heading">--}}
+{{--        Static Content--}}
+{{--    </div>--}}
 
-    <!-- Nav Item - Pages Collapse Menu -->
-    <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
-            <i class="fas fa-fw fa-cog"></i>
-            <span>Components</span>
-        </a>
-        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-            <div class="bg-white py-2 collapse-inner rounded">
-                <h6 class="collapse-header">Custom Components:</h6>
-                <a class="collapse-item" href="buttons.html">Buttons</a>
-                <a class="collapse-item active" href="cards.html">Cards</a>
-            </div>
-        </div>
-    </li>
+{{--    <!-- Nav Item - Pages Collapse Menu -->--}}
+{{--    <li class="nav-item">--}}
+{{--        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">--}}
+{{--            <i class="fas fa-fw fa-cog"></i>--}}
+{{--            <span>Components</span>--}}
+{{--        </a>--}}
+{{--        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">--}}
+{{--            <div class="bg-white py-2 collapse-inner rounded">--}}
+{{--                <h6 class="collapse-header">Custom Components:</h6>--}}
+{{--                <a class="collapse-item" href="buttons.html">Buttons</a>--}}
+{{--                <a class="collapse-item active" href="cards.html">Cards</a>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+{{--    </li>--}}
 
-    <!-- Nav Item - Utilities Collapse Menu -->
-    <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities" aria-expanded="true" aria-controls="collapseUtilities">
-            <i class="fas fa-fw fa-wrench"></i>
-            <span>Utilities</span>
-        </a>
-        <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
-            <div class="bg-white py-2 collapse-inner rounded">
-                <h6 class="collapse-header">Custom Utilities:</h6>
-                <a class="collapse-item" href="utilities-color.html">Colors</a>
-                <a class="collapse-item" href="utilities-border.html">Borders</a>
-                <a class="collapse-item" href="utilities-animation.html">Animations</a>
-                <a class="collapse-item" href="utilities-other.html">Other</a>
-            </div>
-        </div>
-    </li>
+{{--    <!-- Nav Item - Utilities Collapse Menu -->--}}
+{{--    <li class="nav-item">--}}
+{{--        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities" aria-expanded="true" aria-controls="collapseUtilities">--}}
+{{--            <i class="fas fa-fw fa-wrench"></i>--}}
+{{--            <span>Utilities</span>--}}
+{{--        </a>--}}
+{{--        <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">--}}
+{{--            <div class="bg-white py-2 collapse-inner rounded">--}}
+{{--                <h6 class="collapse-header">Custom Utilities:</h6>--}}
+{{--                <a class="collapse-item" href="utilities-color.html">Colors</a>--}}
+{{--                <a class="collapse-item" href="utilities-border.html">Borders</a>--}}
+{{--                <a class="collapse-item" href="utilities-animation.html">Animations</a>--}}
+{{--                <a class="collapse-item" href="utilities-other.html">Other</a>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+{{--    </li>--}}
 
     <!-- Divider -->
     <hr class="sidebar-divider">
@@ -87,20 +87,20 @@
 {{--    </li>--}}
 
     <!-- Nav Item - Charts -->
-    <li class="nav-item">
+    <li class="nav-item @if(Route::currentRouteName()=='programs.index') active @endif">
         <a class="nav-link" href="{{route('programs.index')}}">
             <i class="fas fa-fw fa-code"></i>
             <span>Programs</span></a>
     </li>
 
     <!-- Nav Item - Tables -->
-    <li class="nav-item">
+    <li class="nav-item @if(Route::currentRouteName()=='collections.index') active @endif">
         <a class="nav-link" href="{{route('collections.index')}}">
             <i class="fas fa-fw fa-folder-open"></i>
             <span>Collections</span></a>
     </li>
 
-    <li class="nav-item">
+    <li class="nav-item @if(Route::currentRouteName()=='tests.index') active @endif">
         <a class="nav-link" href="tables.html">
             <i class="fas fa-fw fa-flask"></i>
             <span>Tests</span></a>
